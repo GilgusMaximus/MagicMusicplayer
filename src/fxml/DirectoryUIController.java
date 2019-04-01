50debfd7198e37e62c3da9ba6fc105338eaf7ff9
Luca <hohmann.luca@gmx.de>
2019-04-01 21:44:44 +0200

    Directory UI Usability Added: Directory UI Usability - Every button works as intended, directories can be removed again by clicking (not connected to the input reader yet)

--- src/fxml/DirectoryUIController.java
+++ src/fxml/DirectoryUIController.java
@@ -5,6 +5,7 @@ import java.io.File;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
@@ -15,18 +16,38 @@ public class DirectoryUIController {
    @FXML
    AnchorPane anchorpane;
 
+   @FXML
+   private VBox addedDirectoriesList;
+
    private MusicManager manager;
 
    public void setMusicManager(MusicManager Manager){
       manager = Manager;
    }
 
-   private void browseButtonclicked(){
+   int counter = 0;
+
+   public void addDirectoryToList(){
+      TextField field = new TextField();
+      field.setText(directoryField.getText());
+      field.setId(""+counter++);
+      field.setOnMouseClicked(event -> {delete(field);});
+      field.setEditable(false);
+
+      addedDirectoriesList.getChildren().add(field);
+   }
+   public void delete(TextField field){
+      addedDirectoriesList.getChildren().remove(field);
+   }
+
+
+
+   public void browseButtonclicked(){
       DirectoryChooser chooser = new DirectoryChooser();
       Stage stage = (Stage) anchorpane.getScene().getWindow();
       File file = chooser.showDialog(stage);
       if(file != null){
-         System.out.println("PATH: " + file.getPath());
+         directoryField.setText(file.getPath());
       }
    }
 
